6aadc9f4ae0784f32ecf102ab4cd62af36cf788a
Charles Bessong Tabot <82298650+TabotCharlesBessong@users.noreply.github.com>
2022-12-25 09:34:51 +0100

    Create id.js

---
+++ nodeJs/MongoDb/reference/id.js
@@ -0,0 +1,3 @@
+BSONTypeError: Argument passed in must be a string of 12 bytes or a string of 24 hex characters or an integer
+
+const soution = "use ._id as it is the format for mongodb bson object"
